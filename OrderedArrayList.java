ea379d646b22175673accea53be334d6891fc742
Justin Liu <exovitejl@gmail.com>
2020-12-12 16:04:54 -0500

    added class OrderedArrayList and wrote constructors

---
+++ OrderedArrayList.java
@@ -0,0 +1,11 @@
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+
+  public NoNullArrayList(int startingCapacity){
+    super(startingCapacity);
+  }
+
+  
+}
