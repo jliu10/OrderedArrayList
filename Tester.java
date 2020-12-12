b2e5735f169b0ea7880540d49aaf2cba5f2dcb33
Justin Liu <exovitejl@gmail.com>
2020-12-12 17:28:17 -0500

    tested class OrderedArrayList

--- Tester.java
+++ Tester.java
@@ -2,6 +2,8 @@ public class Tester{
   public static void main(String[] args){
     NoNullArrayList<String> a = new NoNullArrayList<String>();
     NoNullArrayList<Integer> b = new NoNullArrayList<Integer>(5);
+    OrderedArrayList<String> c = new OrderedArrayList<String>(5);
+    OrderedArrayList<Integer> d = new OrderedArrayList<Integer>();
 
     try{
       a.add("nerd");
@@ -12,6 +14,22 @@ public class Tester{
       b.add(2);
       b.set(0,1);
       System.out.println(b);
+
+      c.add("b");
+      c.add("ab");
+      c.add("aa");
+      c.add("hippo");
+      c.add(1,"b");
+      c.set(1,"c");
+      c.add(null);
+      System.out.println(c);
+
+      d.add(1);
+      d.add(100);
+      d.add(-1);
+      d.add(1,100);
+      d.set(1,2);
+      System.out.println(d);
     } catch(IllegalArgumentException e){
         System.out.println("Cannot add null");
     }
